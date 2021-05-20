ea4c7b245492909c56bc59ece74a449acbf60d98
kevin <you@example.com>
2021-05-20 17:08:30 +0300

    Frontend

---
+++ Frontend/index.js
@@ -0,0 +1,14 @@
+var date = new Date()
+document.getElementById('date').innerHTML = date.getFullYear()
+
+const tops = document.querySelector('.top')
+
+window.addEventListener('scroll', ()=> {
+  const scroll_height = window.pageYOffset
+  if (scroll_height > 480 ) {
+    tops.classList.add('top-show')
+  }
+  else {
+    tops.classList.remove('top-show')
+  }
+})
